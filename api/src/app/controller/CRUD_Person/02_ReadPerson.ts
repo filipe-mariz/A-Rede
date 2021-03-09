ebfb456f5af4564d3f8d8992717b46598a970e0e
filipe-mariz <57136731+filipe-mariz@users.noreply.github.com>
2021-03-08 21:51:35 -0300

    View person read

--- api/src/app/controller/CRUD_Person/02_ReadPerson.ts
+++ api/src/app/controller/CRUD_Person/02_ReadPerson.ts
@@ -1,10 +1,17 @@
 import { Request, Response } from 'express';
 import { getRepository } from 'typeorm';
-import * as Yup from 'yup';
+import personView from '../../view/02_Person';
 import Person from '../../model/01_Professional';
 
 export default {
     async view(request: Request, response: Response) {
+        const personRepository = getRepository(Person);
+
+        const person = await personRepository.find({
+            relations: ['image']
+        })
+
+        return response.json(personView.renderMany(person))
 
     },
 
--- api/src/app/controller/CRUD_Professional/02_ReadProfessional.ts
+++ api/src/app/controller/CRUD_Professional/02_ReadProfessional.ts
@@ -1,14 +1,30 @@
 import { Request, Response } from 'express';
 import { getRepository } from 'typeorm';
-import * as Yup from 'yup';
+import professionalView from '../../view/01_Professional';
 import Professional from '../../model/01_Professional';
 
 export default {
     async view(request: Request, response: Response) {
+        const professionalRepository = getRepository(Professional)
 
+        const professional = await professionalRepository.find({
+            relations: ['image']
+        });
+
+        return response.json(professionalView.renderMany(professional));
     },
 
     async show(request: Request, response: Response) {
+        const { id } = request.params
+
+        const professionalRepository = getRepository(Professional)
+
+        const professional = await professionalRepository.findOneOrFail(id, {
+            relations: ['image']
+        });
+
+        return response.json(professionalView.Render(professional));
         
     }
+
 }

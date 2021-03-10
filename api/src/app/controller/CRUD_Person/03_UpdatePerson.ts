b8215a9686b1a89b9f1daff6862205957421ed95
filipe-mariz <57136731+filipe-mariz@users.noreply.github.com>
2021-03-10 09:23:12 -0300

    Update function ready

--- api/src/app/controller/CRUD_Person/03_UpdatePerson.ts
+++ api/src/app/controller/CRUD_Person/03_UpdatePerson.ts
@@ -1,10 +1,17 @@
 import { Request, Response } from 'express';
 import { getRepository } from 'typeorm';
-import * as Yup from 'yup';
 import Person from '../../model/01_Professional';
 
 export default {
     async update(request: Request, response: Response) {
+        const person = await getRepository(Person).findOne(request.params.id)
 
+        if(person) {
+            getRepository(Person).update(person, request.body)
+            const results = getRepository(Person).save(person)
+            return response.json(results)
+        }
+
+        return response.status(401).json({message: "User not found"})        
     }
 }
--- api/src/app/controller/CRUD_Professional/03_UpdateProfessional.ts
+++ api/src/app/controller/CRUD_Professional/03_UpdateProfessional.ts
@@ -1,10 +1,17 @@
 import { Request, Response } from 'express';
 import { getRepository } from 'typeorm';
-import * as Yup from 'yup';
 import Professional from '../../model/01_Professional';
 
 export default {
     async update(request: Request, response: Response) {
-        
+        const professional = await getRepository(Professional).findOne(request.params.id);
+
+        if(professional) {
+            getRepository(Professional).merge(professional, request.body)
+            const results = await getRepository(Professional).save(professional)
+            return response.json({results})
+        }
+
+        return response.send(401).json({message: "User not found"})
     }
 }
